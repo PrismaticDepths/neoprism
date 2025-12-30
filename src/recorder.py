6b69b1b55dff1700c6bb7e8ec1bb80bac010b77e
PrismaticDepths <109403545+PrismaticDepths@users.noreply.github.com>
2025-12-30 15:58:35 -0800

    added proper payloads, enums, and started on the mouse

--- src/recorder.py
+++ src/recorder.py
@@ -1,34 +1,48 @@
 import pynput, time, struct
 
 buffer = bytearray()
+start = 0
 
-start = time.perf_counter_ns()
+class Events: # DO NOT MODIFY EXISTING ENUMS SINCE THAT WOULD BREAK OLD RECORDINGS
+	KEY_DOWN = 10
+	KEY_UP = 11
+	MOUSE_DOWN = 20
+	MOUSE_UP = 21
+	MOUSE_MOVE_ABSOLUTE = 22
+	MOUSE_MOVE_RELATIVE = 23
 
-#EVENT TIMESTAMP, EVENT TYPE, EVENT PAYLOAD
-#UINT64, UINT8, UINT16
-EVENT_FMT = "<QBH"
+#USES LITTLE ENDIAN IF YOU CANT ALREADY TELL
+#HEADER: EVENT TIMESTAMP, EVENT TYPE
+#HEADER: UINT64, UINT8,
+EVENT_HEADER_FMT = "<QB"
+PAYLOAD_FMTS = {
+Events.KEY_DOWN:"<H", #UINT16
+Events.KEY_UP:"<H", #UINT16
+Events.MOUSE_DOWN:"<BHH", #UINT8,UINT16,UINT16
+Events.MOUSE_UP:"<BHH", #UINT8,UINT16,UINT16
+Events.MOUSE_MOVE_ABSOLUTE:"<HH", #UINT16,UINT16
+Events.MOUSE_MOVE_RELATIVE:"<hh", #INT16,INT16
+}
 
-#EVENT TYPES:
-#KEY_DOWN = 0
-#KEY_UP = 1
+def log_event(timestamp,event,payload):
+	buffer.extend(struct.pack(EVENT_HEADER_FMT+PAYLOAD_FMTS[event],timestamp,event,payload))
 
 def captured_key_press(key:pynput.keyboard.Key|pynput.keyboard.KeyCode):
-	tmp=0
-	if isinstance(key,pynput.keyboard.KeyCode):
-		tmp=key.vk
-	elif isinstance(key,pynput.keyboard.Key):
-		tmp=key.value.vk
-	buffer.extend(struct.pack(EVENT_FMT,time.perf_counter_ns()-start,0,tmp))
+	t=time.perf_counter_ns()
+	log_event(t,Events.KEY_DOWN,key.vk if isinstance(key,pynput.keyboard.KeyCode) else key.value.vk)
 def captured_key_release(key:pynput.keyboard.Key|pynput.keyboard.KeyCode):
-	tmp=0
-	if isinstance(key,pynput.keyboard.KeyCode):
-		tmp=key.vk
-	elif isinstance(key,pynput.keyboard.Key):
-		tmp=key.value.vk
-	buffer.extend(struct.pack(EVENT_FMT,time.perf_counter_ns()-start,1,tmp))
-
+	t=time.perf_counter_ns()
+	log_event(t,Events.KEY_UP,key.vk if isinstance(key,pynput.keyboard.KeyCode) else key.value.vk)
+def captured_mouse_click(x,y,button:pynput.mouse.Button,pressed):
+	t=time.perf_counter_ns()
+	log_event(t,Events.MOUSE_DOWN if pressed else Events.MOUSE_UP,button.value,x,y)
+def captured_mouse_move(x,y):
+	t=time.perf_counter_ns()
+	log_event(t,Events.MOUSE_MOVE_ABSOLUTE,x,y)
 kb_listener = pynput.keyboard.Listener(on_press=captured_key_press,on_release=captured_key_release)
+mouse_listener = pynput.mouse.Listener(on_move=captured_mouse_move,)
 
+start = time.perf_counter_ns()
 kb_listener.start()
 time.sleep(10)
 kb_listener.stop()
